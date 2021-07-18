8345d70c3fe5c09d5d4fea1486f4f8e36f339874
angelagongli <angelagongli@gmail.com>
2021-07-18 09:29:46 -0500

    Update IdentifyDesiredStudent.py

--- AnalyzeAllStudentResultData/IdentifyDesiredStudent.py
+++ AnalyzeAllStudentResultData/IdentifyDesiredStudent.py
@@ -12,62 +12,65 @@ cnx = mysql.connector.connect(user='root',
 outerCursor = cnx.cursor(buffered=True)
 innerCursor = cnx.cursor(buffered=True)
 
-student_id = 1
-
 pull_studentinformation = ("SELECT cohort, firstName, middleName, lastName, "
-                            "city, state, country FROM Students WHERE id=%s")
-
-pull_allresultsforstudent = ("SELECT id, resultName "
-                            "FROM StudentResults WHERE student_id=%s")
+                            "city, state, country FROM Students WHERE id=%(student_id)s")
 
 pull_allresultsforstudentatstep = ("SELECT id, resultName, resultAge, resultCity, resultCityHistory "
-                                    "FROM StudentResults WHERE student_id=%s AND identificationStep=%s")
+                                    "FROM StudentResults WHERE student_id=%(student_id)s AND identificationStep=%(identificationStep)s")
 
 update_studentresultidentificationstep = ("UPDATE StudentResults SET identificationStep=%s WHERE id=%s")
 
-outerCursor.execute(pull_studentinformation, student_id)
+for i in range(1, 100):
+    student_data = {
+        'student_id': i,
+        'identificationStep': 'Not Identified'
+    }
 
-for (cohort, firstName, middleName, lastName, city, state, country) in outerCursor:
-    desiredStudentAge = datetime.datetime.now().year - cohort + 22
-    firstName = firstName
-    middleName = middleName
-    lastName = lastName
-    city = city
+    outerCursor.execute(pull_studentinformation, student_data)
 
-outerCursor.execute(pull_allresultsforstudent, student_id)
+    for (cohort, firstName, middleName, lastName, city, state, country) in outerCursor:
+        desiredStudentAge = datetime.datetime.now().year - cohort + 22
+        firstName = firstName
+        middleName = middleName
+        lastName = lastName
+        city = city
 
-for (id, resultName) in outerCursor:
-    if (firstName in resultName.upper() and
-        lastName in resultName.upper()):
-        innerCursor.execute(update_studentresultidentificationstep, ('First Pass', id))
-        cnx.commit()
+    outerCursor.execute(pull_allresultsforstudentatstep, student_data)
 
-outerCursor.execute(pull_allresultsforstudentatstep, (student_id, 'First Pass'))
+    for (id, resultName, resultAge, resultCity, resultCityHistory) in outerCursor:
+        if (firstName in resultName.upper() and
+            lastName in resultName.upper()):
+            innerCursor.execute(update_studentresultidentificationstep, ('First Pass', id))
+            cnx.commit()
 
-for (id, resultName, resultAge, resultCity, resultCityHistory) in outerCursor:
-    if (middleName in resultName.upper() or
-        f" {middleName[0:1]} " in resultName.upper()):
-        innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
-        cnx.commit()
-    elif (resultAge >= desiredStudentAge - 1 and
-       resultAge <= desiredStudentAge + 1):
-        innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
-        cnx.commit()
-    elif city in resultCityHistory.upper():
-        innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
-        cnx.commit()
+    student_data['identificationStep'] = 'First Pass'
+    outerCursor.execute(pull_allresultsforstudentatstep, student_data)
 
-outerCursor.execute(pull_allresultsforstudentatstep, (student_id, 'Refined'))
+    for (id, resultName, resultAge, resultCity, resultCityHistory) in outerCursor:
+        if middleName and (middleName in resultName.upper() or
+            f" {middleName[0:1]} " in resultName.upper()):
+            innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
+            cnx.commit()
+        elif resultAge and (resultAge >= desiredStudentAge - 1 and
+            resultAge <= desiredStudentAge + 1):
+            innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
+            cnx.commit()
+        elif city and city in resultCityHistory.upper():
+            innerCursor.execute(update_studentresultidentificationstep, ('Refined', id))
+            cnx.commit()
 
-for (id, resultName, resultAge, resultCity, resultCityHistory) in outerCursor:
-    if ((middleName in resultName.upper() or
-        f" {middleName[0:1]} " in resultName.upper()) and
-        (resultAge >= desiredStudentAge - 1 and
-        resultAge <= desiredStudentAge + 1) and
-        city in resultCityHistory.upper()):
-        innerCursor.execute(update_studentresultidentificationstep, ('Identified', id))
-        cnx.commit()
-        break
+    student_data['identificationStep'] = 'Refined'
+    outerCursor.execute(pull_allresultsforstudentatstep, student_data)
+
+    for (id, resultName, resultAge, resultCity, resultCityHistory) in outerCursor:
+        if ((middleName and (middleName in resultName.upper() or
+            f" {middleName[0:1]} " in resultName.upper())) and
+            (resultAge and (resultAge >= desiredStudentAge - 1 and
+            resultAge <= desiredStudentAge + 1)) and
+            (city and city in resultCityHistory.upper())):
+            innerCursor.execute(update_studentresultidentificationstep, ('Identified', id))
+            cnx.commit()
+            break
 
 innerCursor.close()
 outerCursor.close()
